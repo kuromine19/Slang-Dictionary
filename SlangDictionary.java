07f470d899dfc39ee363fa0daaa33966e4533a90
kuromine19 <85854041+kuromine19@users.noreply.github.com>
2022-03-19 00:36:12 +0700

    Update SlangDictionary.java

--- SlangDictionary.java
+++ SlangDictionary.java
@@ -1,5 +1,22 @@
 package source;
 
-public class SlangDictionary {
+//import java.util.Collections;
+//import java.util.Scanner;
+//import java.util.Comparator;
 
+public class SlangDictionary{
+	public static void Menu(){
+		System.out.println("\t\tMENU");
+		System.out.println("1. Find definition by slang word.");
+		System.out.println("2. Find sland words by definiton.");
+		System.out.println("3. History.");
+		System.out.println("4. Add slang word.");
+		System.out.println("5. Edit slang word.");
+		System.out.println("6. Reset dictionary.");
+		System.out.println("7. Random a slang word.");
+		System.out.println("8. Slang Quiz.");
+	}
+	public static void main(String[] args) {
+		Menu();
+	}
 }
