931b2c12504e1799ffa3dae21724bf1110598141
kuromine19 <85854041+kuromine19@users.noreply.github.com>
2022-03-27 10:51:49 +0700

    Update SlangDictionary.java

--- SlangDictionary.java
+++ SlangDictionary.java
@@ -1,10 +1,51 @@
 package source;
 
-//import java.util.Collections;
-//import java.util.Scanner;
-//import java.util.Comparator;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class SlangDictionary{
+public HashMap<String,String> map=new HashMap<String,String>();
+    
+    public static void getSlangWord()
+	{
+		
+		try
+		{
+			File file = new File(".Data/slang.txt");
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			String data;
+			while((data = br.readLine())!=null)
+			{
+				if(data.contains("`"))
+				{
+					String[] info= data.split("`");
+					String[] def = info[1].split("\\|");
+					for(int i=0;i<def.length;i++)
+					{
+						def[i]=def[i].trim();
+						
+					}
+					
+				}
+			}
+		    fr.close();
+	        br.close();
+	    }
+	    catch (Exception ex)
+	    {
+	        System.out.println("ERROR"+ex);
+	    }
+		
+		
+	}
 	public static void Menu(){
 		System.out.println("\t\tMENU");
 		System.out.println("1. Find definition by slang word.");
